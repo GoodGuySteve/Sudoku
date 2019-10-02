7aedbe9e9446eec9ebac70bc271ac96780e87a95
GoodGuySteve <q-bert@live.com>
2019-10-01 20:58:57 -0700

    Finished the basic Board class with the ability to populate and print a sudoku board from a 2D array.

--- sudoku/src/main.rs
+++ sudoku/src/main.rs
@@ -12,11 +12,35 @@ struct Board {
 impl Board {
 	fn new_from_array(arr: [[u32; SCALE_FACTOR * SCALE_FACTOR]; SCALE_FACTOR * SCALE_FACTOR])
 	-> Board {
-		Board { entries: [[Entry { value: 0 }; SCALE_FACTOR * SCALE_FACTOR]; SCALE_FACTOR * SCALE_FACTOR]}
+		/* Instantiate a new Board given a 2D array of numbers to fill with */
+		let mut board = Board { entries: [[Entry { value: 0 }; SCALE_FACTOR * SCALE_FACTOR]; SCALE_FACTOR * SCALE_FACTOR]};
+		
+		let mut i: usize;
+		let mut j: usize;
+		i = 0;
+		for row in board.entries.iter_mut() {
+			j = 0;
+			for square in row.iter_mut() {
+				square.value = arr[i][j];
+				j += 1;
+			}
+			i += 1;
+		}
+		return board;
 	}
 	
 	fn print(self) {
-		
+		/* Prints out the entire board in a pretty format. */
+		/* Note that this print statement is probably inefficient. Consider preallocation if 
+		   this is slow. */
+		for row in self.entries.iter() {
+			let mut row_str = String::from("");
+			for square in row.iter() {
+				row_str.push_str(&square.value.to_string());
+				row_str.push_str(", ")
+			}
+			println!("{}", row_str);
+		}
 	}
 }
 
@@ -32,7 +56,7 @@ fn main() {
 		[9, 1, 2, 3, 4, 5, 6, 7, 8]];
 
 	let test_board = Board::new_from_array(test_array);
-	test_board.print()
+	test_board.print();
 
     println!("Hello, world!");
 }
