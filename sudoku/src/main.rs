403296c1b8f418e9d2e2ef2bacffeaa28790467e
GoodGuySteve <q-bert@live.com>
2021-04-17 21:38:41 -0700

    Added the function to validate the board state.

--- sudoku/src/main.rs
+++ sudoku/src/main.rs
@@ -1,19 +1,47 @@
 const SCALE_FACTOR: usize = 3;
+const NUMBER_LIMIT: usize = SCALE_FACTOR * SCALE_FACTOR;
+const DEBUG: bool = true;
+
+fn box_num(row: usize, col: usize) -> usize {
+	/* Takes the zero-indexed row and column number and returns the zero-indexed box number of the given entry. Boxes
+	are numbered from left to right, then top to bottom. 
+	
+	Example: for a 3x3, box 0 is top left, box 2 is top right, box 6 is bottom left, 
+	box 8 is bottom right.
+	*/
+	let box_row: usize = row / SCALE_FACTOR;
+	let box_col: usize = col / SCALE_FACTOR;
+
+	return SCALE_FACTOR * box_row + box_col;
+}
 
 #[derive(Copy, Clone)]
 struct Entry {
-	value: u32,
+	value: usize,
+	row: usize,
+	col: usize,
+	boxx: usize,
 }
 
 struct Board {
-	entries: [[Entry; SCALE_FACTOR * SCALE_FACTOR]; SCALE_FACTOR * SCALE_FACTOR],
+	entries: [[Entry; NUMBER_LIMIT]; NUMBER_LIMIT],
+}
+
+impl Entry {
+
 }
 
 impl Board {
-	fn new_from_array(arr: [[u32; SCALE_FACTOR * SCALE_FACTOR]; SCALE_FACTOR * SCALE_FACTOR])
+	fn new_from_array(arr: [[usize; NUMBER_LIMIT]; NUMBER_LIMIT])
 	-> Board {
 		/* Instantiate a new Board given a 2D array of numbers to fill with */
-		let mut board = Board { entries: [[Entry { value: 0 }; SCALE_FACTOR * SCALE_FACTOR]; SCALE_FACTOR * SCALE_FACTOR]};
+		let mut board = Board { 
+			entries: [
+				[
+					Entry { value: 0, row: 0, col: 0, boxx: 0 }; NUMBER_LIMIT
+				]; NUMBER_LIMIT
+			]
+		};
 		
 		let mut i: usize;
 		let mut j: usize;
@@ -22,6 +50,9 @@ impl Board {
 			j = 0;
 			for square in row.iter_mut() {
 				square.value = arr[i][j];
+				square.row = i;
+				square.col = j;
+				square.boxx = box_num(i, j);
 				j += 1;
 			}
 			i += 1;
@@ -29,7 +60,67 @@ impl Board {
 		return board;
 	}
 	
-	fn print(self) {
+	fn is_valid(&self) -> bool {
+		/* Checks if the current board is a valid Sudoku board according to Sudoku rules
+		   (each number appears exactly once in each row, column, and subsquare). Unset
+		   entries are considered valid.
+		*/ 
+
+		/* Start with each row, column, and box empty. Iterate through each entry of the sudoku and check for collisions */
+		/* Position is used to map to numeric entry value */
+		let mut rows: [[bool; NUMBER_LIMIT]; NUMBER_LIMIT] = [[false; NUMBER_LIMIT]; NUMBER_LIMIT];
+		let mut cols: [[bool; NUMBER_LIMIT]; NUMBER_LIMIT] = [[false; NUMBER_LIMIT]; NUMBER_LIMIT];
+		let mut boxes: [[bool; NUMBER_LIMIT]; NUMBER_LIMIT] = [[false; NUMBER_LIMIT]; NUMBER_LIMIT];
+
+		for row in self.entries.iter() {
+			for entry in row.iter() {
+				if (entry.value - 1) > NUMBER_LIMIT {
+					/* This is out of range, so an error is more appropriate */
+					/* TODO we should make a function to validate all of the entry fields. Just in case.*/
+					println!("{}, {} contains invalid entry.", entry.row, entry.col);
+					return false;
+				} else if entry.value != 0 {
+					/* 0 counts as empty */
+
+					if rows[entry.row][entry.value - 1] == true {
+						/* We've seen this entry in this row already */
+						if DEBUG {
+							println!("{}, {}: {} already present in row {}.", entry.row, entry.col, entry.value, entry.row);
+						}
+						return false;
+					} else {
+						/* Mark that we've seen this entry in this row */
+						rows[entry.row][entry.value - 1] = true;
+					}
+					if cols[entry.col][entry.value  - 1] == true {
+						/* We've seen this entry in this column already */
+						if DEBUG {
+							println!("{}, {}: {} already present in column {}.", entry.row, entry.col, entry.value, entry.col);
+						}
+						return false;
+					} else {
+						/* Mark that we've seen this entry in this column */
+						cols[entry.col][entry.value - 1] = true;
+					}
+					if boxes[entry.boxx][entry.value - 1] == true {
+						/* We've seen this entry in this box already */
+						if DEBUG {
+							println!("{}, {}: {} already present in box {}.", entry.row, entry.col, entry.value, entry.boxx);
+						}
+						return false;
+					} else {
+						/* Mark that we've seen this entry in this box */
+						boxes[entry.boxx][entry.value - 1] = true;
+					}
+				}
+
+			}
+		}
+
+		return true;
+	}
+	
+	fn print(&self) {
 		/* Prints out the entire board in a pretty format. */
 		/* Note that this print statement is probably inefficient. Consider preallocation if 
 		   this is slow. */
@@ -46,7 +137,7 @@ impl Board {
 
 fn main() {
 	let test_array = [[1, 2, 3, 4, 5, 6, 7, 8, 9],
-	    [4, 5, 6, 7, 8, 8, 1, 2, 3],
+	    [4, 5, 6, 7, 8, 9, 1, 2, 3],
 		[7, 8, 9, 1, 2, 3, 4, 5, 6],
 		[2, 3, 4, 5, 6, 7, 8, 9, 1],
 		[5, 6, 7, 8, 9, 1, 2, 3, 4],
@@ -58,5 +149,5 @@ fn main() {
 	let test_board = Board::new_from_array(test_array);
 	test_board.print();
 
-    println!("Hello, world!");
+    println!("{}", test_board.is_valid());
 }
